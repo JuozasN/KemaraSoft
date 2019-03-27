cb6fc3fef1f171a5a27699fc57896c910a97f93d
ArnasStep <arnas.steponavicius@gmail.com>
2019-03-27 18:54:59 +0200

    VM updates

--- src/Word.java
+++ src/Word.java
@@ -1,13 +1,21 @@
 public class Word {
 	public static final int BYTE_COUNT = 4;
 	private byte[] bytes;
+	private void resetBytes(){
+		byte[] empty = {0, 0, 0, 0};
+		bytes = empty;
+	}
 	
 	public Word(){
 		bytes = new byte[BYTE_COUNT];
 	}
 	
 	public byte[] getBytes(){return bytes;}
-	public void setBytes(byte[] bytes){System.arraycopy(bytes, 0, this.bytes, 0, bytes.length);}
+	public void setBytes(byte[] bytes){resetBytes(); System.arraycopy(bytes, 0, this.bytes, 0, bytes.length);}
 	
-	public String toString(){return new String(bytes);}
+	public String toString(){
+		String str = new String(bytes);
+		str = str.replaceAll("\0", "");
+		return str;
+	}
 }
