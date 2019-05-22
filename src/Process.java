8b58bf378feb94bea154730ed4c01b541e49527b
ArnasStep <arnas.steponavicius@gmail.com>
2019-05-22 17:02:31 +0300

    Stuff

--- src/Process.java
+++ src/Process.java
@@ -11,47 +11,71 @@ public class Process {
     }
 
     private ArrayList<Process> processList = new ArrayList<>();
-    private int ID;
+    private static long previousID = 0;
+    private long ID;
     private Short[] savedRegisters = new Short[7];
-    private ArrayList<Resource> createdResources = new ArrayList<>();
+    private ArrayList<Resource> createdResources;
     private ArrayList<Resource> ownedResources = new ArrayList<>();
+    private ArrayList<Resource> elementList;
     private byte state;
     private byte priority;
     private Process parent;
     private ArrayList<Process> children;
     private String title;
 
-    public Process(Process parent, byte state, ArrayList<Resource> elementList, String title) {
+    public Process(){
+        this.ID = previousID++;
+    }
+
+    public void create(Process parent, byte state, byte priority, ArrayList<Resource> elementList, String title) {
         this.parent = parent;
         this.state = state;
-        // TODO: baigti inicializuoti (kurti procesą)
+        this.priority = priority;
+        this.elementList = elementList;
+        this.title = title;
+        //TODO: Add to Process List
+        parent.addToChildren(this);
+        children = new ArrayList<>();
+        createdResources = new ArrayList<>();
     }
 
     public void delete() {
-
+        for(Resource r: createdResources){
+            r.delete();
+        }
+        for(Process c: children){
+            c.delete();
+        }
+        parent.removeFromChildren(this);
+        //TODO: Remove from Process List
+        for(Resource r: ownedResources){
+            r.delete();
+        }
     }
 
     public void suspend() {
-
+        if(state < 3){
+            state+=3;
+        }
+        else {
+            //Do something
+        }
     }
 
     public void activate() {
-
+        if(state >= 3){
+            state-=3;
+        }
+        else {
+            //Do something
+        }
     }
 
-    public void addToCreatedResources(Resource resource) {
-        this.createdResources.add(resource);
+    public void addToChildren(Process children){
+        this.children.add(children);
     }
 
-    public void removeFromCreatedResources(Resource resource) {
-        this.createdResources.remove(resource);
-    }
-
-    public void removeFromOwnedResources(Resource resource) {
-        this.ownedResources.remove(resource);
-    }
-
-    public void changeState(byte state) {
-        this.state = state;
+    public void removeFromChildren(Process children){
+        this.children.remove(children);
     }
 }
