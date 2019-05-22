039cb65dbc5575e6afe7cbf97078dc1e496fd8bd
ArnasStep <arnas.steponavicius@gmail.com>
2019-05-22 16:05:39 +0300

    Stuff

--- src/Process.java
+++ src/Process.java
@@ -6,36 +6,76 @@ public class Process {
         public static final byte BLOCKED = 1;
         public static final byte READY = 2;
         public static final byte SUSPENDED = 3;
-        public static final byte BLOCKED_SUSPENDED = 3;
-        public static final byte READY_SUSPENDED = 3;
+        public static final byte BLOCKED_SUSPENDED = 4;
+        public static final byte READY_SUSPENDED = 5;
     }
 
     private ArrayList<Process> processList = new ArrayList<>();
-    private int ID;
+    private static long previousID = 0;
+    private long ID;
     private Short[] savedRegisters = new Short[7];
-    private ArrayList<Resource> createdResources = new ArrayList<>();
+    private ArrayList<Resource> createdResources;
     private ArrayList<Resource> ownedResources = new ArrayList<>();
+    private ArrayList<Resource> elementList;
     private byte state;
     private byte priority;
     private Process parent;
     private ArrayList<Process> children;
     private String title;
 
-    public Process(Process parent, byte state, ArrayList<Resource> elementList, String title) {
+    public Process(){
+        this.ID = previousID++;
+    }
+
+    public void create(Process parent, byte state, byte priority, ArrayList<Resource> elementList, String title) {
         this.parent = parent;
         this.state = state;
-        // TODO: baigti inicializuoti (kurti procesą)
+        this.priority = priority;
+        this.elementList = elementList;
+        this.title = title;
+        //TODO: Add to Process List
+        parent.addToChildren(this);
+        children = new ArrayList<>();
+        createdResources = new ArrayList<>();
     }
 
-    public static void delete() {
-
+    public void delete() {
+        for(Resource r: createdResources){
+            r.delete();
+        }
+        for(Process c: children){
+            c.delete();
+        }
+        parent.removeFromChildren(this);
+        //TODO: Remove from Process List
+        for(Resource r: ownedResources){
+            r.delete();
+        }
     }
 
-    public static void suspend() {
-
+    public void suspend() {
+        if(state < 3){
+            state+=3;
+        }
+        else {
+            //Do something
+        }
     }
 
-    public static void activate() {
+    public void activate() {
+        if(state >= 3){
+            state-=3;
+        }
+        else {
+            //Do something
+        }
+    }
 
+    public void addToChildren(Process children){
+        this.children.add(children);
+    }
+
+    public void removeFromChildren(Process children){
+        this.children.remove(children);
     }
 }
