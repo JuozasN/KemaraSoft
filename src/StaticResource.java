5a74a2342afa3671de62c92caf0076f945e38682
Juozas <43491708+JuozasN@users.noreply.github.com>
2019-05-29 20:23:49 +0300

    Merge branch 'Stepukas' of https://github.com/JuozasN/KemaraSoft into Juozas
    # Conflicts:
    #	src/LoadProgram.java

--- src/StaticResource.java
+++ src/StaticResource.java
@@ -1,2 +1,40 @@
+import java.util.Arrays;
+
 public class StaticResource extends Resource {
+    private Block[] elementList;
+
+    public void create(Process creator, String title, int eListSize){
+        this.elementList = new Block[eListSize];
+        super.create(creator, title);
+    }
+
+    public Block[] getElementList() {
+        return this.elementList;
+    }
+
+    public void delete() {
+        this.clearElementList();
+        super.delete();
+        //"naikinamas pats aprasas"...
+    }
+
+    public void clearElementList() {
+        Arrays.fill(this.elementList, null);
+    }
+
+    public void release(Block element) {
+        // perduodame elementą resursui ir kviečiame paskirstytoją,
+        // kad jis perduotų elementą jo laukiančiam procesui
+        this.addLast(element);
+        super.release();
+    }
+
+    public void addLast(Block element){
+        for (int i = 0; i < this.elementList.length; i++) {
+            if (this.elementList[i] == null){
+                this.elementList[i] = element;
+                break;
+            }
+        }
+    }
 }
