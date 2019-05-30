cffd46700a7213596a8c141dbd761ea50ef18136
Juozas <43491708+JuozasN@users.noreply.github.com>
2019-05-30 18:57:57 +0300

    Added appendLog in most class methods.

--- src/Process.java
+++ src/Process.java
@@ -153,7 +153,7 @@ public abstract class Process {
 //        this.ownedResources.remove(resource);
 //    }
 
-//    public void addToOwnedResources(Resource resource) { this.ownedResources.add(resource); }
+    public void addToOwnedResources(Resource resource) { this.ownedResources.add(resource); }
 
 //    public void addToOwnedResources(Resource resource) {
 //        this.ownedResources.addAll(resource.getElementList());
